73f0ae49131ad86f218b447a05379c62d91f2b6d
Maria <maria.rudolphi@hotmail.com>
2018-10-03 14:16:50 +0200

    Version 1.2 - clean & comment code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+// declare require to use require in typescript
 declare function require(name:string);
 var fs = require('fs');
 
+// define class to read files, takes name as an parameter
 class FilePublisher {
     filename: string;
 
@@ -8,33 +10,35 @@ class FilePublisher {
         this.filename = name;
     }
 
+    // function to read and show file
     showFile(): void {
         fs.readFile(this.filename, 'utf8', function(err, data) {
             if (err) {
                 throw err;
             }
             else {
+                // split file content in array including all words
                 var clean: string[];
                 clean = data.split(/\W+/g);
-                // /\n|[ ,."!?:;/*]+/
-                let count = {};
 
+                // count how many times every word is repeated
+                let count = {};
                 for (let i of clean) {
                     count[i] = (count[i]||0) + 1;
                 }
 
+                // add all words with at least 10 letters to array sorted
                 let sorted = [];
-
                 for (let j in count) {
                     if (j.length >= 10) {
                         sorted.push([j, count[j]]);
                     }
                 }
 
+                // sort words and reverse, most used word first, write 10 most used words
                 sorted.sort(function (a, b) {
                     return a[1] - b[1];
                 });
-
                 sorted.reverse();
 
                 console.log('Det här är de 10 mest förekommande orden med minst 10 tecken ur boken "The hitchhickers guide to the galaxy"');
@@ -44,5 +48,6 @@ class FilePublisher {
     }
 }
 
+// define new FilePublisher with filename 'src/hitch.txt' and call showFile() to write 10 most used words with at least 10 letters
 let obj = new FilePublisher('src/hitch.txt');
 obj.showFile();
